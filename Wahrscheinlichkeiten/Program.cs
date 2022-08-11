8299d77ad33752766ffc51248ebcce563a9f4642
Mohammad El-Masri <Mohammad.El-Masri@fairmas.com>
2022-08-11 12:44:09 +0200

    komplett fertig

--- Wahrscheinlichkeiten/Program.cs
+++ Wahrscheinlichkeiten/Program.cs
@@ -7,28 +7,29 @@ using System.Threading.Tasks;
 //es soll 100 x mit 10%, 20% usw bis 90% spielen. dann soll er mir sagen wie oft er von 100x spielen gewonnen hat.
 //bei welcher prozentzahl ist die wahrscheinlichkeit am höchsten zu gewinnen
 
-//er spielt 100 bei 10% und sagt zb: 10 mal gewonnen
-//dann spielt er 100 bei 20% und sagt zb: 20 ma gewonnen
-//usw bis 90%
-//Am ende haben wir 9 Werte. Automatisiert soll nun ausgegeben werden, bei welkcher prozentzahl es am wahrscheinlichsten ist zu gewinnen
+//er spielt 100x bei 10% und sagt zb: 2 mal gewonnen
+//dann spielt er 100 bei 20% und sagt zb: 3 ma gewonnen usw bis 90% hoch
+//Am ende haben wir 9 Werte. Automatisiert soll nun ausgegeben werden, bei welcher prozentzahl es am wahrscheinlichsten ist zu gewinnen
 
-//Ich habe 100 karten
-//10% spiele ich, heißt mein set sind 10 karten drinne.
+//100 Zahlen sind in der Liste
+//10% spiele ich, heißt mein set sind 10 Zahlen drinne.
 //dann ziehe ich weiter, wenn eine karte kommt die kleiner ist als die max zahl im set, ziehe ich weiter
 //ziehe ich eine zahl die größer ist als meine max zahl im set, dann sage ich das ist die max zahl
-//und prüfe dann meine currentlyNumberAfterSet mit maxnumber in list. siend diese == = win, wenn nicht lost
+//dann prüfe ich meine currentlyNumberAfterSet mit maxnumber in list. sind diese == = win, wenn nicht lost
 
+
+//Wenn die zweit größte Zahl im Set ist und die größe Außerhalb des SETS, dann haben wir immer ein WIN!
 namespace Wahrscheinlichkeiten
 {
     internal class Program
     {
         private static List<int> _listOfNumbers = new List<int>();
-        private static List<Winning> _listOfWinnings = new List<Winning>();
+        private static List<Winning> _listEndOfWinnings = new List<Winning>();
 
         private static Random _random = new Random();
 
         private const int _min = 0;
-        private const int _max = 11;
+        private const int _max = 1001;
 
         private static int _turns = 0;
         private static int _currentlyNumberAfterSet = 0;
@@ -36,13 +37,14 @@ namespace Wahrscheinlichkeiten
         private static int _maxNumberInList = 0;
         private static int _numberOfPercent = 0;
         private static int _i = 0;
-        private static int _getCurrentNumber = 0;
+        private static int _tempCurrentNumber = 0;
         private static int _numberOfWinnings = 0;
 
         static void Main(string[] args)
         {
             while(_numberOfPercent < 90)
             {
+                _numberOfWinnings = 0;
                 Console.WriteLine();
                 Console.WriteLine();
                 Console.WriteLine();
@@ -62,7 +64,7 @@ namespace Wahrscheinlichkeiten
                     _maxNumberInList = GetMaxNumber();
 
                     _turns = DetermineTurns(_numberOfPercent);
-                    _maxNumberInSet = DetermineMaxNumberInSet();
+                    _maxNumberInSet = GetMaxNumberInSet();
 
                     Console.WriteLine("MAX NUMBER IN SET: " + _maxNumberInSet);
 
@@ -83,18 +85,18 @@ namespace Wahrscheinlichkeiten
                     {
                         Console.WriteLine($"L O S T. CurrentlyNumber: {_currentlyNumberAfterSet} MaxNumberInList: {_maxNumberInList} index: {_i}.");
                     }
-
+                    
                     Console.WriteLine("Round: " + j + " --------------------------------------------------------------------------------------------------");
                 }
 
                 Console.WriteLine("Number of winnings: " + _numberOfWinnings);
-                Console.WriteLine();
+                Console.WriteLine(); 
                 Winning winning = new Winning(_numberOfPercent, _numberOfWinnings);
-                _listOfWinnings.Add(winning);
+                _listEndOfWinnings.Add(winning);
             }
 
             ShowAllOfWinnings();
-            var highestNumber = GetHighestProbabilityOfWinning(_listOfWinnings);
+            var highestNumber = GetHighestProbabilityOfWinning(_listEndOfWinnings);
             Console.WriteLine();
             Console.WriteLine("The highest probability of winning is at " + highestNumber.SetPercent + "% with: " +  highestNumber.NumberOfWinnings);
 
@@ -106,12 +108,12 @@ namespace Wahrscheinlichkeiten
             var number = 0;
             Winning obj = null;
 
-            foreach (var item in _list)
+            foreach (var objInList in _list)
             {
-                if(item.NumberOfWinnings > number)
+                if(objInList.NumberOfWinnings >= number)
                 {
-                    number = item.NumberOfWinnings;
-                    obj = item;
+                    number = objInList.NumberOfWinnings;
+                    obj = objInList;
                 }
             }
             
@@ -120,7 +122,7 @@ namespace Wahrscheinlichkeiten
 
         private static void ShowAllOfWinnings()
         {
-            foreach (var item in _listOfWinnings)
+            foreach (var item in _listEndOfWinnings)
             {
                 Console.WriteLine("At " + item.SetPercent + "%" + " there were " + item.NumberOfWinnings + " wins.");
             }
@@ -131,7 +133,6 @@ namespace Wahrscheinlichkeiten
             _listOfNumbers.Clear();
             _maxNumberInSet = 0;
             _maxNumberInList = 0;
-            _numberOfWinnings = 0;
             _i = 0;
         }
 
@@ -145,18 +146,18 @@ namespace Wahrscheinlichkeiten
             return false;
         }
 
-        private static int DetermineMaxNumberInSet()
+        private static int GetMaxNumberInSet()
         {
 
             while (_i < _turns)
             {
-                _getCurrentNumber = _listOfNumbers[_i];
+                _tempCurrentNumber = _listOfNumbers[_i];
 
                 _i++;
 
-                if (_getCurrentNumber > _maxNumberInSet)
+                if (_tempCurrentNumber > _maxNumberInSet)
                 {
-                    _maxNumberInSet = _getCurrentNumber;
+                    _maxNumberInSet = _tempCurrentNumber;
                 }
             }
 
